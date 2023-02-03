3f76efb96685deeb2f067e6d96da18fae10d17fa
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-02-03 16:37:20 +0100

    Update script.js
    if applied, this commit will add a new item to the list

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+    }
+}
